2729d1f56a60faa31916a32985e8919a547de5a0
ali-85 <alidavit85#gmail.com>
2024-07-24 09:11:53 +0700

    Certificate file upload

--- app/Http/Controllers/API/CertificateController.php
+++ app/Http/Controllers/API/CertificateController.php
@@ -12,42 +12,43 @@ class CertificateController extends Controller
 {
     public function store(Request $request)
     {
-        
+
         $request->validate([
             'certificate' => 'required|file|max:2048',
         ]);
-    
+
         // Dapatkan file yang diupload
         $file = $request->file('certificate');
-    
+
         // Buat path penyimpanan dengan nama file asli
         $fileName = $file->getClientOriginalName();
-        $path = $file->storeAs('certificates', $fileName);
-    
+        // $path = $file->storeAs('certificates', $fileName);
+        $file->move(public_path('certificates/'), $fileName);
+
         // Ambil data sertifikat dari user yang sedang login
         $certificate = Auth::user()->certificate;
-    
+
         if ($certificate) {
             // Hapus file sertifikat lama
-            Storage::delete($certificate->certificate_path);
+            // Storage::delete($certificate->certificate_path);
+            unlink('certificates/'.$fileName);
             // Update sertifikat dengan path baru
             $certificate->update([
-                'certificate_path' => $path,
+                'certificate_path' => $fileName,
             ]);
         } else {
             // Buat entri sertifikat baru
             Certificate::create([
                 'user_id' => Auth::id(),
-                'certificate_path' => $path,
+                'certificate_path' => $fileName,
             ]);
         }
-    
+
         // Kembalikan respons JSON
         return response()->json([
             'success' => true,
             'message' => 'Sertifikat berhasil diunggah!',
-            'certificate_path' => $path
         ], 200);
-    
+
     }
 }
