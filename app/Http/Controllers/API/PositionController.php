4672a1b2b4ca1fc69e0a8e0a7c38d6a4aba87e13
ali-85 <alidavit85#gmail.com>
2024-08-05 13:53:57 +0700

    position using Auth id

--- app/Http/Controllers/API/PositionController.php
+++ app/Http/Controllers/API/PositionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Position;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class PositionController extends Controller
@@ -13,46 +14,71 @@ class PositionController extends Controller
     public function getData()
     {
         $data = Position::all();
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Data retrieved Successfully',
-            'data' => $data,
-        ], 200);
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'Data retrieved Successfully',
+                'data' => $data,
+            ],
+            200
+        );
     }
-    public function getLastPositionUser($id_user)
+    public function getLastPositionUser(Request $request)
     {
-        $data = User::with('position')->find($id_user);
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Data retrieved Successfully',
-            'data' => $data
-        ], 200);
+        if ($request->has('id_user')) {
+            $data = User::with('position')->find($request->id_user);
+        } else {
+            $data = User::with('position')->get();
+        }
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'Data retrieved Successfully',
+                'data' => $data,
+            ],
+            200
+        );
     }
     public function store(Request $request)
     {
-        $input = $request->only(['user_id', 'latitude', 'longitude']);
-        $validator = Validator::make($input, Position::rules(), [], Position::attributes());
+        $input = $request->only(['latitude', 'longitude']);
+        $validator = Validator::make(
+            $input,
+            Position::rules(),
+            [],
+            Position::attributes()
+        );
         if ($validator->fails()) {
-            return response()->json([
-                'status' => 'error',
-                'fields' => $validator->errors()
-            ], 200);
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'fields' => $validator->errors(),
+                ],
+                200
+            );
         } else {
+            $input['user_id'] = Auth::id();
             $user = User::find($input['user_id']);
             $position = Position::create($input);
             if ($position) {
                 $user->update([
-                    'position_id' => $position->id
+                    'position_id' => $position->id,
                 ]);
-                return response()->json([
-                    'status' => 'success',
-                    'message' => 'Position create successfully'
-                ], 200);
+                return response()->json(
+                    [
+                        'status' => 'success',
+                        'message' => 'Position create successfully',
+                    ],
+                    200
+                );
             } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'internal server error'
-                ], 500);
+                return response()->json(
+                    [
+                        'status' => 'error',
+                        'message' => 'internal server error',
+                    ],
+                    500
+                );
             }
         }
     }
