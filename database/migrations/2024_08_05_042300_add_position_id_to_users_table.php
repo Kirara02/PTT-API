7478a9cf1114fc3881af81af3b80369411c14008
ali-85 <alidavit85#gmail.com>
2024-08-06 08:50:23 +0700

    default position id 0

--- database/migrations/2024_08_05_042300_add_position_id_to_users_table.php
+++ database/migrations/2024_08_05_042300_add_position_id_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('position_id');
+            $table->unsignedBigInteger('position_id')->default(0);
         });
     }
 
