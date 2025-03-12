c8ac84c73cde7948ac240c2ab61007f933b73b90
ali-85 <alidavit85#gmail.com>
2025-03-12 12:30:11 +0700

    fix: filter markers by authenticated user's company ID

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Company;
 use App\Models\Server;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -26,6 +27,9 @@ class DashboardController extends Controller
             ->join('timezones as tz', 'tz.id', '=', 'co.timezone_id')
             ->has('position')
             ->with('position')
+            ->when(Auth::user()->level_id != 0, function ($query) {
+                return $query->where('users.company_id', Auth::user()->company_id);
+            })
             ->get();
         return response()->json([
             'status' => 'success',
